71e284232527b4164e2b202965879a56e4134801
Sandy Averhoff <bierhoff@get.tur.cu>
2018-01-05 09:17:17 -0500

    - disabled button when is working..

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,19 +18,19 @@
                       
                       <div class="form-group">
                         <label>Subject</label>
-                        <input type="text" class="form-control" placeholder="Subject" v-model="subject">
+                        <input :disabled="working" type="text" class="form-control" placeholder="Subject" v-model="subject">
                       </div>    
                       <div class="form-group">
                         <label>Body</label>  
-                        <textarea class="form-control" v-model="body" placeholder="Body content"></textarea>
+                        <textarea :disabled="working" class="form-control" v-model="body" placeholder="Body content"></textarea>
                       </div>
                       <label>Emails</label>  
                       <div class="form-group" v-for="(i, k) in emails">                       
-                        <input type="email" class="form-control" placeholder="Email" v-model="emails[k]">
+                        <input type="email" :disabled="working" class="form-control" placeholder="Email" v-model="emails[k]">
                       </div>    
                       
-                      <button type="button" @click="add()" class="btn btn-default">Add email</button>
-                      <button type="button" @click="send()" class="btn btn-primary">Send Email</button>
+                      <button type="button" :disabled="working" @click="add()" class="btn btn-default">Add email</button>
+                      <button type="button" :disabled="working" @click="send()" class="btn btn-primary">Send Email</button>
 
                     </form>
 
@@ -53,7 +53,8 @@
                 return {              
                     emails: [''],
                     subject: '',
-                    body: ''
+                    body: '',
+                    working: false;
                 };              
             },
             methods: {
@@ -61,13 +62,16 @@
                     this.emails.push(''); 
                 },
                 send: function() {                     
+                    this.working = true;
                     var data = { subject: this.subject, body: this.body, to: this.emails[0], cc:this.emails.slice(1) };
-                      $.ajax({type: "POST", contentType: 'application/json', dataType: 'json', url: '/api/send-email', data: JSON.stringify(data)}).then(this.onEmail, this.onError);
+                    $.ajax({type: "POST", contentType: 'application/json', dataType: 'json', url: '/api/send-email', data: JSON.stringify(data)}).then(this.onEmail, this.onError);
                 },
                 onError: function(e) {                     
+                    this.working = false;
                     alert(e.responseJSON.message);
                 },
                 onEmail: function(d) { 
+                    this.working = false;
                     alert('Email send ( sweetalert maybe.. )');
                 }
             }
